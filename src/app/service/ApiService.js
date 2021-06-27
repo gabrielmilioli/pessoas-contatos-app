ca5a7176f2d16170c9a373d8c21ecf27cf2870a4
Gabriel <gmilioli@outlook.com>
2021-06-27 15:25:06 -0300

    Redireciona api url para Heroku

--- src/app/service/ApiService.js
+++ src/app/service/ApiService.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const HTTP_CLIENT = axios.create({
-  baseURL: 'http://localhost:8080/api'
+  //baseURL: 'http://localhost:8080/api'
+  baseURL: 'https://milioli-pessoascontatos-api.herokuapp.com/api'
 });
 
 class ApiService {
